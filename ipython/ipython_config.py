1624e3663ec5b0b7e1679aae585a9d288757d75c
Luke Lee <durdenmisc@gmail.com>
2013-03-04 11:01:16 -0600

    Add line profiler extensions for ipython

--- ipython/ipython_config.py
+++ ipython/ipython_config.py
@@ -2,6 +2,13 @@
 
 c = get_config()
 
+c.TerminalIPythonApp.extensions = [
+    'line_profiler_ext',
+]
+c.InteractiveShellApp.extensions = [
+    'line_profiler_ext',
+]
+
 #------------------------------------------------------------------------------
 # InteractiveShellApp configuration
 #------------------------------------------------------------------------------
