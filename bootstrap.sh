7401965b3b5fa7f9ddd0f70524b770e159a20064
Luke Lee <durdenmisc@gmail.com>
2014-12-02 09:51:09 -0600

    Add applescripts and bootstrap shell script to setup work env after reboot

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Start up all iterm sessions
+osascript /Users/lukelee/dotfiles/open_work_shells.scpt
+
+# Start up all closed vim sessions
+osascript /Users/lukelee/dotfiles/open_vim_sessions.scpt
+
+# Start up all apps I always want running
+osascript /Users/lukelee/dotfiles/open_apps.scpt
