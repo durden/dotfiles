b6b458bd25d58a143586f83fc5a0be4334207516
Luke Lee <durdenmisc@gmail.com>
2015-07-16 10:51:21 +0200

    Add git aliases that take author as argument - from macpro branch

--- git_aliases.sh
+++ git_aliases.sh
@@ -1,41 +1,27 @@
 # Fun git shortcuts/stats snatched from various places online
-gitimpact()
+gitimpacttotal()
 {
-    res=`/usr/bin/git log --numstat --pretty="%H" --author=llee "$@" | awk 'NF==3 {plus+=$1; minus+=$2} END {printf("+%d, -%d\n", plus, minus)}'`
+    author="$1"
+    leftovers="${*:2}"
+    res=`/usr/bin/git log --numstat --pretty="%H" --author="$author" $leftovers | awk 'NF==3 {plus+=$1; minus+=$2} END {printf("+%d, -%d\n", plus, minus)}'`
     added=`echo $res | cut -d, -f1 | cut -d+ -f2`
     removed=`echo $res | cut -d, -f2 | cut -d- -f2`
     net=`expr $added - $removed`
     echo "Net: $net (Added: $added Removed: $removed)"
 }
 
-# Short cut to get pretty view of what commits in current branch are NOT yet
-# in remote svn repo, aka what will be pushed with a git svn dcommit cmd
-gitsvnpushed()
+gitimpactpy()
 {
-    # Stash anything that we currently have outstanding
-    stashed=0
-    if [ ! -z "`git diff --shortstat`" ]; then
-        echo 'STASHED'
-        stashed=1
-        git stash -q
-    fi
+    author="$1"
+    leftovers="${*:2}"
 
-    commits=`git svn dcommit --dry-run | wc -l`
-
-    # Output has one line of header information
-    pushed_commits=`expr $commits - 1`
-
-    echo "$pushed_commits commits to be pushed"
-
-    git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit -n$pushed_commits "$@"
-    gitsvndiffstats
-
-    # Get local changes back
-    if [ $stashed -eq 1 ]; then
-        echo 'UNSTASHED'
-        git stash pop -q
-    fi
+    res=`/usr/bin/git log --numstat --pretty="%H" --author=$author $leftovers -- \*.py| awk 'NF==3 {plus+=$1; minus+=$2} END {printf("+%d, -%d\n", plus, minus)}'`
+    added=`echo $res | cut -d, -f1 | cut -d+ -f2`
+    removed=`echo $res | cut -d, -f2 | cut -d- -f2`
+    net=`expr $added - $removed`
+    echo "Net: $net (Added: $added Removed: $removed)"
 }
+
 
 # Short cut to get pretty view of what commits in current branch are NOT yet
 # in remote svn repo, aka what will be pushed with a git svn dcommit cmd
@@ -98,7 +84,7 @@ changelog() {
     git log HEAD...`git log CHANGELOG | grep commit | cut -d' ' -f2 | head -n1`
 }
 
-alias gi=gitimpact
+alias gi=gitimpacttotal
 alias gsd=gitsvnpushed
 alias gss=gitsvndiffstats
 alias gwhackfile=whack_file
