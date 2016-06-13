7452dc13e40132fb5be7f428e850db6baa2a3814
Luke Lee <durdenmisc@gmail.com>
2016-06-13 22:43:05 +0200

    Cleanup python paths, etc.

--- virtualenv_path.sh
+++ virtualenv_path.sh
@@ -1,7 +1,9 @@
 # virtualenv
 VIRTUALENV_DIR="/Users/durden/.virtualenvs"
-VIRTUALENVWRAPPER_PYTHON=/usr/local/Cellar/python/2.7.3/bin/python
+#VIRTUALENVWRAPPER_PYTHON=/Users/durden/anaconda/bin/python
+#VIRTUALENVWRAPPER_PYTHON=/usr/local/Cellar/python/2.7.3/bin/python
+VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
 export VIRTUALENVWRAPPER_PYTHON
 export VIRTUALENV_DIR
 export WORKON_HOME=$VIRTUALENV_DIR
-source /usr/local/share/python/virtualenvwrapper.sh
+source /usr/local/bin/virtualenvwrapper.sh
