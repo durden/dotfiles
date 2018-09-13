3dc42a416a71ca3135482d2a7cb8477ca757f833
Luke Lee <durdenmisc@gmail.com>
2018-09-13 13:35:31 +0200

    Update virtualenvwrapper path once installing python 2 and 3 on same machine

--- virtualenv_path.sh
+++ virtualenv_path.sh
@@ -2,7 +2,7 @@
 VIRTUALENV_DIR="/Users/durden/.virtualenvs"
 #VIRTUALENVWRAPPER_PYTHON=/Users/durden/anaconda/bin/python
 #VIRTUALENVWRAPPER_PYTHON=/usr/local/Cellar/python/2.7.3/bin/python
-VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
+VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python2
 export VIRTUALENVWRAPPER_PYTHON
 export VIRTUALENV_DIR
 export WORKON_HOME=$VIRTUALENV_DIR
